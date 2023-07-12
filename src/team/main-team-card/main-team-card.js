2c2058750e38266a8ecfeef9418a061bdf4e99ad
Kosyan2917 <kosyankov01@gmail.com>
2023-07-12 17:52:42 +0300

    Версия декора через заголовок и бордер

---
+++ src/team/main-team-card/main-team-card.js
@@ -0,0 +1,9 @@
+import "./main-team-card.css"
+
+const MainTeamCard = () => {
+    <div>
+        
+    </div>
+}
+
+export default MainTeamCard
--- src/team/main_frame/team.js
+++ src/team/main_frame/team.js
@@ -3,6 +3,13 @@ import "./team.css"
 const Team = () => {
     return <div className={"flex"}>
         <h3> Наша дружная команда</h3>
+        <h1 className={"team-dots"}> ... </h1>
+        <div className={"team-border-top"}>
+        </div>
+        <div className={"team-border-right"}>
+        </div>
+        <div className={"team-grid"}>
+        </div>
     </div>
 
 }
