51d157e1e2a0a0cf9c150152301c75539781ee53
Kosyan2917 <kosyankov01@gmail.com>
2023-07-12 22:57:06 +0300

    Версия декора через гриды

--- src/team/main_frame/team.js
+++ src/team/main_frame/team.js
@@ -1,8 +1,29 @@
 import "./team.css"
 
 const Team = () => {
-    return <div className={"flex"}>
-        <h3> Наша дружная команда</h3>
+    return <div className={"team"}>
+        <div className={"team-header-grid"}>
+        <h1> Наша дружная команда </h1>
+        <div className={"team-dots"}>
+            <div></div>
+            <div className={"team-dot"}>
+            </div>
+            <div></div>
+            <div className={"team-dot"}></div>
+            <div></div>
+            <div className={"team-dot"}></div>
+            <div></div>
+            <div className={"team-grid-line"}>
+                <div></div>
+                <div className={"team-line"}></div>
+                <div></div>
+            </div>
+            <div className={"team-vertical-line"}></div>
+        </div>
+        </div>
+        <div className={"team-grid"}>
+
+        </div>
     </div>
 
 }
